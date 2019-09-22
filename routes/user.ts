9a40fb30bcdb9b03dc48b1895c04c7517871db27
Guido Pontet <guidopontet@gmail.com>
2019-09-22 00:32:22 -0300

    Encrypt password before creating user

--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { User } from "../models/user.model";
+import bcrypt from "bcrypt";
 
 const userRoutes = Router();
 
@@ -7,7 +8,7 @@ userRoutes.post( '/', (req: Request, res: Response) => {
   const user = {
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: bcrypt.hashSync(req.body.password, 10),
     avatar: req.body.avatar
   };
 

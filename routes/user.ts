374332c2783aa89f5caec7ce422975cbdef7b7d0
Guido Pontet <guidopontet@gmail.com>
2019-09-22 00:25:50 -0300

    Create user

--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,31 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { User } from "../models/user.model";
 
 const userRoutes = Router();
+
+userRoutes.post( '/', (req: Request, res: Response) => {
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    avatar: req.body.avatar
+  };
+
+  User.create( user )
+    .then( newUser => {
+      res.json({
+        ok: true,
+        user: newUser
+      });
+    })
+    .catch( err => {
+      res.json({
+        ok: false,
+        err,
+      });
+    });
+
+
+});
 
 export default userRoutes;

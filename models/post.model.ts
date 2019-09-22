ba0ab0bcce860bb6a72df01c327fdba6aa200683
Guido Pontet <guidopontet@gmail.com>
2019-09-22 20:56:02 -0300

    Add post model

---
+++ models/post.model.ts
@@ -0,0 +1,37 @@
+import { Schema, Document, model } from "mongoose";
+
+const postSchema = new Schema({
+  created: {
+    type: Date,
+  },
+  message: {
+    type: String,
+  },
+  img: [{
+    type: String,
+  }],
+  coords: {
+    type: String
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [ true, 'La relación con el tipo "User" es necesaria'],
+  }
+});
+
+// Seteamos la fecha antes de insertar el documento en la base de datos
+postSchema.pre<IPost>('save', function(next) {
+  this.created = new Date();
+  next();
+});
+
+interface IPost extends Document {
+  created: Date;
+  message: string;
+  img: string[];
+  coords: string;
+  user: string;
+}
+
+export const Post = model<IPost>('Post', postSchema);

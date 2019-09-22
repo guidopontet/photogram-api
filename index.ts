44b68abb610eb638960cf675591090110088f84c
Guido Pontet <guidopontet@gmail.com>
2019-09-21 23:50:14 -0300

    Add mongodb connection

--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/user";
+import mongoose from "mongoose";
 
 const server = new Server();
 
+// Routes
 server.app.use( '/user', userRoutes);
 
+// DB
+mongoose.connect( 'mongodb://localhost:27017/photogram',
+                  {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true},
+                  err => {
+                    if ( err ) throw err;
+
+                    console.log(`Database running on port: 27017`);
+                  });
+
+// Express
 server.start();

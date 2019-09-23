92007bc906296a890c2bfd9a1101b0a972362f07
Guido Pontet <guidopontet@gmail.com>
2019-09-22 21:34:18 -0300

    Get pagina posts

--- routes/post.ts
+++ routes/post.ts
@@ -4,7 +4,26 @@ import { Post } from "../models/post.model";
 
 const postRoutes = Router();
 
-// Crear post
+// Get posts
+postRoutes.get('/', async (req: any, res: Response) => {
+  let page = Number(req.query.page) || 1;
+  let skip = (page - 1) * 10;
+
+  const posts = await Post.find()
+                          .sort({_id: -1})
+                          .skip(skip)
+                          .limit(10)
+                          .populate('user', '-password')
+                          .exec();
+
+  res.json({
+    ok: true,
+    page,
+    posts
+  });
+});
+
+// Create post
 postRoutes.post('/', validateToken, (req: any, res: Response) => {
   const body = req.body;
   body.user = req.user._id;
